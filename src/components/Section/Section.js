927016852e1c3a3e639b56223cc682ed5c168062
MarynaIlchenko <debovamail@gmail.com>
2022-05-08 01:18:15 +0300

    update

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,21 +1,21 @@
-import React, {Component} from "react";
-import style from './Section.module.css';
+import React, { Component } from 'react';
+import './Section.module.css';
 import PropTypes from 'prop-types';
 
-class Section extends Component{
-    render() {
-        const { title, children } = this.props;
-        return (
-            <div className={StyleSheet.Section}>
-                <h2 className="Title">{title}</h2>
-                {children}
-            </div>
-        )
-    }
-};
-Section.propTypes = {
-    title: PropTypes.string.isRequired,
-    children: PropTypes.element.isRequired,
+class Section extends Component {
+  render() {
+    const { title, children } = this.props;
+    return (
+      <div className={StyleSheet.Section}>
+        <h2 className="Title">{title}</h2>
+        {children}
+      </div>
+    );
+  }
 }
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
 
 export default Section;
