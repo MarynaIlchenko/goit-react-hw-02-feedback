ae869207e19d0a03a9946a41877fb27ae3d1a3f9
MarynaIlchenko <debovamail@gmail.com>
2022-05-14 15:58:36 +0300

    update Notification

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Notification extends Component {
-  render() {
-    const { message } = this.props;
-    return (
-      <div className="Notification">
-        <span>{message}</span>
-      </div>
-    );
-  }
-}
+const Notification = ({ message }) => {
+  return (
+    <div className="Notification">
+      <span>{message}</span>
+    </div>
+  );
+};
+
+// class Notification extends Component {
+//   render() {
+//     const { message } = this.props;
+//     return (
+//       <div className="Notification">
+//         <span>{message}</span>
+//       </div>
+//     );
+//   }
+// }
 
 Notification.propTypes = {
-  message: PropTypes.string,
+  message: PropTypes.string.isRequired,
 };
 
 export default Notification;
